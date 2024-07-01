a30b31da230173431a708a21b11f79a08e22da6c
jjhy129 <33469549+jjhy129@users.noreply.github.com>
2024-06-30 21:47:53 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const searchButton = document.querySelector('.search-button');
-    const musicRecommendations = document.querySelector('.music-recommendations');
-
-    searchButton.addEventListener('click', function() {
-        musicRecommendations.style.display = 'block';
-    });
-});
